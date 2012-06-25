f21788e49482e250fddc5d6f6b71efc421e9ce5a
Tom Flanagan <knio@zkpq.ca>
2012-06-24 22:45:45 -0700

    renumber pins

--- Arduino/examples/Canbus/mcp2515.c
+++ Arduino/examples/Canbus/mcp2515.c
@@ -28,6 +28,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "WConstants.h"
 #include "global.h"
 #include "mcp2515.h"
 #include "mcp2515_defs.h"
@@ -42,11 +43,11 @@ uint8_t spi_putc( uint8_t data )
 {
 	// put byte in send-buffer
 	SPDR = data;
-
+	
 	// wait until byte was send
 	while( !( SPSR & (1<<SPIF) ) )
 		;
-
+	
 	return SPDR;
 }
 
@@ -54,11 +55,11 @@ uint8_t spi_putc( uint8_t data )
 void mcp2515_write_register( uint8_t adress, uint8_t data )
 {
 	RESET(MCP2515_CS);
-
+	
 	spi_putc(SPI_WRITE);
 	spi_putc(adress);
 	spi_putc(data);
-
+	
 	SET(MCP2515_CS);
 }
 
@@ -66,16 +67,16 @@ void mcp2515_write_register( uint8_t adress, uint8_t data )
 uint8_t mcp2515_read_register(uint8_t adress)
 {
 	uint8_t data;
-
+	
 	RESET(MCP2515_CS);
-
+	
 	spi_putc(SPI_READ);
 	spi_putc(adress);
-
-	data = spi_putc(0xff);
-
+	
+	data = spi_putc(0xff);	
+	
 	SET(MCP2515_CS);
-
+	
 	return data;
 }
 
@@ -83,12 +84,12 @@ uint8_t mcp2515_read_register(uint8_t adress)
 void mcp2515_bit_modify(uint8_t adress, uint8_t mask, uint8_t data)
 {
 	RESET(MCP2515_CS);
-
+	
 	spi_putc(SPI_BIT_MODIFY);
 	spi_putc(adress);
 	spi_putc(mask);
 	spi_putc(data);
-
+	
 	SET(MCP2515_CS);
 }
 
@@ -96,63 +97,63 @@ void mcp2515_bit_modify(uint8_t adress, uint8_t mask, uint8_t data)
 uint8_t mcp2515_read_status(uint8_t type)
 {
 	uint8_t data;
-
+	
 	RESET(MCP2515_CS);
-
+	
 	spi_putc(type);
 	data = spi_putc(0xff);
-
+	
 	SET(MCP2515_CS);
-
+	
 	return data;
 }
 
 // -------------------------------------------------------------------------
 uint8_t mcp2515_init(uint8_t speed)
 {
-
-
+		
+	
 	SET(MCP2515_CS);
 	SET_OUTPUT(MCP2515_CS);
-
+	
 	RESET(P_SCK);
 	RESET(P_MOSI);
 	RESET(P_MISO);
-
+	
 	SET_OUTPUT(P_SCK);
 	SET_OUTPUT(P_MOSI);
 	SET_INPUT(P_MISO);
-
+	
 	SET_INPUT(MCP2515_INT);
 	SET(MCP2515_INT);
-
+	
 	// active SPI master interface
 	SPCR = (1<<SPE)|(1<<MSTR) | (0<<SPR1)|(1<<SPR0);
 	SPSR = 0;
-
+	
 	// reset MCP2515 by software reset.
 	// After this he is in configuration mode.
 	RESET(MCP2515_CS);
 	spi_putc(SPI_RESET);
 	SET(MCP2515_CS);
-
+	
 	// wait a little bit until the MCP2515 has restarted
 	_delay_us(10);
-
+	
 	// load CNF1..3 Register
 	RESET(MCP2515_CS);
 	spi_putc(SPI_WRITE);
 	spi_putc(CNF3);
-
+	
 /*	spi_putc((1<<PHSEG21));		// Bitrate 125 kbps at 16 MHz
 	spi_putc((1<<BTLMODE)|(1<<PHSEG11));
 	spi_putc((1<<BRP2)|(1<<BRP1)|(1<<BRP0));
 */
-/*
+/*	
 	spi_putc((1<<PHSEG21));		// Bitrate 250 kbps at 16 MHz
 	spi_putc((1<<BTLMODE)|(1<<PHSEG11));
 	spi_putc((1<<BRP1)|(1<<BRP0));
-*/
+*/	
 	spi_putc((1<<PHSEG21));		// Bitrate 250 kbps at 16 MHz
 	spi_putc((1<<BTLMODE)|(1<<PHSEG11));
 	//spi_putc(1<<BRP0);
@@ -161,24 +162,24 @@ uint8_t mcp2515_init(uint8_t speed)
 	// activate interrupts
 	spi_putc((1<<RX1IE)|(1<<RX0IE));
 	SET(MCP2515_CS);
-
+	
 	// test if we could read back the value => is the chip accessible?
 	if (mcp2515_read_register(CNF1) != speed) {
 		SET(LED2_HIGH);
 
 		return false;
 	}
-
+	
 	// deaktivate the RXnBF Pins (High Impedance State)
 	mcp2515_write_register(BFPCTRL, 0);
-
+	
 	// set TXnRTS as inputs
 	mcp2515_write_register(TXRTSCTRL, 0);
-
+	
 	// turn off filters => receive any message
 	mcp2515_write_register(RXB0CTRL, (1<<RXM1)|(1<<RXM0));
 	mcp2515_write_register(RXB1CTRL, (1<<RXM1)|(1<<RXM0));
-
+	
 	// reset device to normal mode
 	mcp2515_write_register(CANCTRL, 0);
 //	SET(LED2_HIGH);
@@ -198,12 +199,12 @@ uint8_t mcp2515_check_message(void) {
 uint8_t mcp2515_check_free_buffer(void)
 {
 	uint8_t status = mcp2515_read_status(SPI_READ_STATUS);
-
+	
 	if ((status & 0x54) == 0x54) {
 		// all buffers used
 		return false;
 	}
-
+	
 	return true;
 }
 
@@ -229,26 +230,26 @@ uint8_t mcp2515_get_message(tCAN *message)
 
 	RESET(MCP2515_CS);
 	spi_putc(addr);
-
+	
 	// read id
 	message->id  = (uint16_t) spi_putc(0xff) << 3;
 	message->id |=            spi_putc(0xff) >> 5;
-
+	
 	spi_putc(0xff);
 	spi_putc(0xff);
-
+	
 	// read DLC
 	uint8_t length = spi_putc(0xff) & 0x0f;
-
+	
 	message->header.length = length;
 	message->header.rtr = (bit_is_set(status, 3)) ? 1 : 0;
-
+	
 	// read data
 	for (t=0;t<length;t++) {
 		message->data[t] = spi_putc(0xff);
 	}
 	SET(MCP2515_CS);
-
+	
 	// clear interrupt flag
 	if (bit_is_set(status, 6)) {
 		mcp2515_bit_modify(CANINTF, (1<<RX0IF), 0);
@@ -256,7 +257,7 @@ uint8_t mcp2515_get_message(tCAN *message)
 	else {
 		mcp2515_bit_modify(CANINTF, (1<<RX1IF), 0);
 	}
-
+	
 	return (status & 0x07) + 1;
 }
 
@@ -264,7 +265,7 @@ uint8_t mcp2515_get_message(tCAN *message)
 uint8_t mcp2515_send_message(tCAN *message)
 {
 	uint8_t status = mcp2515_read_status(SPI_READ_STATUS);
-
+	
 	/* Statusbyte:
 	 *
 	 * Bit	Function
@@ -280,7 +281,7 @@ uint8_t mcp2515_send_message(tCAN *message)
 	}
 	else if (bit_is_clear(status, 4)) {
 		address = 0x02;
-	}
+	} 
 	else if (bit_is_clear(status, 6)) {
 		address = 0x04;
 	}
@@ -288,18 +289,18 @@ uint8_t mcp2515_send_message(tCAN *message)
 		// all buffer used => could not send message
 		return 0;
 	}
-
+	
 	RESET(MCP2515_CS);
 	spi_putc(SPI_WRITE_TX | address);
-
+	
 	spi_putc(message->id >> 3);
     spi_putc(message->id << 5);
-
+	
 	spi_putc(0);
 	spi_putc(0);
-
+	
 	uint8_t length = message->header.length & 0x0f;
-
+	
 	if (message->header.rtr) {
 		// a rtr-frame has a length, but contains no data
 		spi_putc((1<<RTR) | length);
@@ -307,21 +308,21 @@ uint8_t mcp2515_send_message(tCAN *message)
 	else {
 		// set message length
 		spi_putc(length);
-
+		
 		// data
 		for (t=0;t<length;t++) {
 			spi_putc(message->data[t]);
 		}
 	}
 	SET(MCP2515_CS);
-
+	
 	_delay_us(1);
-
+	
 	// send message
 	RESET(MCP2515_CS);
 	address = (address == 0) ? 1 : address;
 	spi_putc(SPI_RTS | address);
 	SET(MCP2515_CS);
-
+	
 	return address;
 }
