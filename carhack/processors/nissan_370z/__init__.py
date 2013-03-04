8f7ef10ca3a0187efda1b859950335333147b0bc
Tom Flanagan <theknio@gmail.com>
2013-03-03 22:06:12 -0800

    debug nissan processor

--- carhack/processors/nissan_370z/__init__.py
+++ carhack/processors/nissan_370z/__init__.py
@@ -43,13 +43,13 @@ class Nissan370ZProcessor(Processor):
     self.publish('steering_position', ts, steering_position)
 
   def can_180(self, ts, A, B, C, D, E, F, G, H):
-    rpm = signed_short(B, A)
+    rpm = signed_short(A, B)
     throtle_position = percent(F)
     self.publish('rpm_a', ts, rpm)
     self.publish('throtle_position', ts, throtle_position)
 
   def can_1f9(self, ts, A, B, C, D, E, F, G, H):
-    rpm = signed_short(D, C)
+    rpm = signed_short(C, D)
     self.publish('rpm_b', ts, rpm)
 
   def can_215(self, ts, A, B, C, D, E, F):
@@ -61,16 +61,16 @@ class Nissan370ZProcessor(Processor):
     self.publish('clutch_pedal_to_floor_a', ts, clutch_pedal_to_floor)
 
   def can_280(self, ts, A, B, C, D, E, F, G, H):
-    vehicle_speed = unsigned_short(F, E)
+    vehicle_speed = unsigned_short(E, F)
     self.publish('vehicle_speed_a', ts, vehicle_speed)
 
   def can_351(self, ts, A, B, C, D, E, F, G, H):
     clutch_pedal_to_floor = bit(H & 0x04)
-    self.publish('clutch_pedal_to_floor_a', ts, clutch_pedal_to_floor)
+    self.publish('clutch_pedal_to_floor_b', ts, clutch_pedal_to_floor)
 
   def can_354(self, ts, A, B, C, D, E, F, G, H):
-    tcs_indicator = bit(~E & 0x40)
-    brake_light   = bit( G & 0x10)
+    tcs_indicator = bit(E & 0x80)
+    brake_light   = bit(G & 0x10)
     self.publish('tcs_indicator', ts, tcs_indicator)
     self.publish('brake_light', ts, brake_light)
 
@@ -82,7 +82,7 @@ class Nissan370ZProcessor(Processor):
       224: 3,
     }.get(C, 255)
     car_moving  = bit(E == 0x40)
-    car_stopped = bit(E == 0x08)
+    car_stopped = bit(E == 0x10)
     self.publish('wiper_status', ts, wiper_status)
     self.publish('car_moving', ts, car_moving)
     self.publish('car_stopped', ts, car_stopped)
@@ -104,12 +104,12 @@ class Nissan370ZProcessor(Processor):
     sensor = A
     engine_revolutions = B
 
-    crusie_control_master_a = bit(E & 0x01)
+    # crusie_control_master_a = bit(E & 0x01)
     crusie_control_master_b = bit(F & 0x50)
 
     self.publish('unknown_sensor_551_a', ts, sensor)
     self.publish('engine_revolutions', ts, engine_revolutions)
-    self.publish('crusie_control_master_a', ts, crusie_control_master_a)
+    # self.publish('crusie_control_master_a', ts, crusie_control_master_a)
     self.publish('crusie_control_master_b', ts, crusie_control_master_b)
 
   def can_580(self, ts, A, B, C, D, E):
